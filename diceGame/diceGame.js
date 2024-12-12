7945479294639255a3f0641e446c80a7b4fbd111
lufftewaffle <leeando90@gmail.com>
2024-12-12 19:31:15 +1300

    'added animation for dice being rolled'

--- diceGame/diceGame.js
+++ diceGame/diceGame.js
@@ -1,7 +1,19 @@
 const rollButton = document.querySelector( '#roll-button' );
+const player1Dice = document.querySelector( '.player1' );
+const player2Dice = document.querySelector( '.player2' );
+
 
 rollButton.addEventListener( 'click', ()=>{
-    setTimeout(()=>{
+        player1Dice.classList.add( 'dice1Animation' );
+        player2Dice.classList.add( 'dice2Animation' );
+
+
+        player1Dice.addEventListener( 'animationend', () => { /*removes animation class to be retriggered once complete.  */
+            player1Dice.classList.remove( 'dice1Animation' );
+          });
         
-    }, 100)
+          player2Dice.addEventListener( 'animationend', () => {
+            player2Dice.classList.remove( 'dice2Animation' );
+          });
+
 })
